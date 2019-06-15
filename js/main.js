e4e74a739b969075eb05e8b1353c01c0a920c673
zinchandrey <zinchandrey@gmail.com>
2019-06-15 12:28:29 +0300

    Выполняет пп 1. и 2. задания

--- js/main.js
+++ js/main.js
@@ -1,22 +1,51 @@
 'use strict';
 
-function getRandom() {
-  return Math.random();
+var AVATAR_TEMPLATE = 'img/avatars/user0';
+var AVATAR_EXTENSION = '.png';
+var OBJECTS_COUNT = 8;
+var HOUSE_TYPES = ['palace', 'flat', 'house', 'bungalo'];
+var Y_FROM = 130;
+var Y_TO = 630;
+
+
+var map = document.querySelector('.map--faded');
+// console.log(map.classList);
+map.classList.remove('map--faded');
+
+function getBlockWidth(blockClass) {
+  return document.querySelector(blockClass).offsetWidth;
 }
 
-var announcements = [
-  {
-    autor: {
-      avatar: 'img/avatars/user0' + Math.round(getRandom() * 8) + '.png'
+function getRandomElement(arr) {
+  return arr[Math.floor(Math.random() * arr.length)];
+}
+
+function getRandomNumber(from, to) {
+  return Math.floor(Math.random() * (to - from)) + from;
+}
+
+function generateObject(avatarNumber) {
+  return {
+    author: {
+      avatar: AVATAR_TEMPLATE + avatarNumber + AVATAR_EXTENSION,
     },
     offer: {
-      type: 'palace'
+      type: getRandomElement(HOUSE_TYPES),
     },
     location: {
-      x: 150,
-      y: 50
+      x: getRandomNumber(0, getBlockWidth('.map__pins')),
+      y: getRandomNumber(Y_FROM, Y_TO),
     }
-  }
-];
+  };
+}
 
-console.log(announcements);
+function generateData(objectsCount) {
+  var data = [];
+
+  for (var i = 0; i < OBJECTS_COUNT; i++) {
+    data[i] = generateObject(i + 1);
+    // Math.floor(Math.random()*(i + 1));
+    // data.push(generateObject(i + 1));
+  }
+  return data;
+}
