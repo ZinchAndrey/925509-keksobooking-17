4f3b32fdb34069a233bb8a09d3a61dae3cd128b5
zinchandrey <zinchandrey@gmail.com>
2019-07-21 22:19:41 +0300

    Исправляет код по аналогии с демкой

--- js/load.js
+++ js/load.js
@@ -1,26 +1,15 @@
 'use strict';
 
 (function () {
-  window.load = function () {
+  window.load = function (onSuccess, onError) {
 
     var URL = 'https://js.dump.academy/keksobooking/data';
     var xhr = new XMLHttpRequest();
-    // xhr.responseType = 'json';
+    xhr.responseType = 'json';
     xhr.open('GET', URL);
 
-    xhr.addEventListener('load', function (evt) {
-      var xhrArray = JSON.parse(xhr.responseText);
-
-      // console.log(evt.target === xhr);
-      // console.log(xhr.readyState);
-      // console.log(xhr.status + ' ' + xhr.statusText);
-      // console.log(xhrArray);
-
-      // try {
-      //   console.log(xhrArray);
-      // } catch (err) {
-      //   console.error('err.message'); // использовать блок error из шаблона template
-      // }
+    xhr.addEventListener('load', function () {
+      onSuccess(xhr.response);
     });
 
     xhr.send();
